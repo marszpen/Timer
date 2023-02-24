e1639b4f4c462dea641be168087f2d8208464774
Karina Penkala <marsz@poczta.onet.pl>
2023-02-24 13:20:55 +0100

    fix TimeConvert

--- src/components/TimeConverter/TimeConverter.js
+++ src/components/TimeConverter/TimeConverter.js
@@ -1,6 +1,6 @@
 const TimeConverter = (props) => {
-    const msToTime = (milliseconds) => {
-    const ms = Math.floor((milliseconds % 1000) / 100);
+    const msToTime = (miliseconds) => {
+    const ms = Math.floor((miliseconds % 1000) / 100);
       seconds = Math.floor((miliseconds / 1000) % 60);
       minutes = Math.floor((miliseconds / (1000 * 60)) % 60);
       hours = Math.floor((miliseconds / (1000 * 60 * 60)) % 24);
